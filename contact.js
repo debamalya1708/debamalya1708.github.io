4f16878b2ba93e42b59f0882cebce1d9b72e5936
Debamalya Palai <debamalyapaloi2001@gmail.com>
2021-07-18 12:23:45 +0530

    initial commit

---
+++ contact.js
@@ -0,0 +1,14 @@
+// function contact(){
+//     alert("Message sent successfully");
+//     event.preventDefault;
+// }
+
+
+$(document).ready(function(){
+    $("form").on("submit", function(event){
+        alert("Message sent successfully");
+        event.preventDefault();
+
+
+    });
+});
